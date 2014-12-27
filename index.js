64bccfaeef6a7e47d8a83022ce248d8cf8437f09
Isheeta Shah <isheeta_s@yahoo.com>
2014-12-27 01:57:28 -0600

    added stuff

---
+++ index.js
@@ -0,0 +1,67 @@
+var aboutBtn = document.getElementById("About");//gives each information element a variable//
+var expBtn = document.getElementById("Experience");
+var wrkBtn = document.getElementById("Work");
+var cntBtn = document.getElementById("Contact");
+var copy = document.getElementById("copyright");
+$(aboutBtn).hide();//hiding all the elements on load//
+$(expBtn).hide();
+$(wrkBtn).hide();
+$(cntBtn).hide();
+function change1(){//hide all the elements except for the About, fade that in at medium speed and change the background image//
+	$(aboutBtn).fadeIn("medium");
+	$(expBtn).hide();
+	$(wrkBtn).hide();
+	$(cntBtn).hide();
+	$(copy).hide();
+	document.body.style.backgroundImage="url('http://i.imgur.com/KZuPcKH.jpg')";
+}
+function change2(){//hide all the elements except for the Experience, fade that in at medium speed//
+	$(aboutBtn).hide();
+	$(expBtn).fadeIn("medium");
+	$(wrkBtn).hide();
+	$(cntBtn).hide();
+	$(copy).hide();
+	document.body.style.backgroundImage="url('http://i.imgur.com/5slMcmh.jpg')";/*document.body.style.backgroundImage="url('http://i.imgur.com/nX3sy7F.jpg')"; document.body.style.backgroundImage="url('http://i.imgur.com/R4pQOBs.jpg')"; */
+}
+function change3(){//hide all the elements except for the Work, fade that in at medium speed//
+	$(expBtn).hide();
+	$(aboutBtn).hide();
+	$(wrkBtn).fadeIn("medium");
+	$(cntBtn).hide();
+	$(copy).hide();
+}
+function change4(){//hide all the elements except for the Contact, fade that in at medium speed//
+	$(expBtn).hide();
+	$(aboutBtn).hide();
+	$(wrkBtn).hide();
+	$(cntBtn).fadeIn("medium");
+	$(copy).hide();
+}
+function change5(){//hide all the elements and change the background image//
+	$(expBtn).hide();
+	$(aboutBtn).hide();
+	$(wrkBtn).hide();
+	$(cntBtn).hide();
+	$(copy).show();
+	document.body.style.backgroundImage="url('http://i.imgur.com/Yh7pIC3.jpg')";
+}
+var about = document.getElementById("about-click");//create variable that is assigned to the about button on the nav bar//
+about.addEventListener('click', function(event) {//attach the change1 function to the about button; excutes it when about is clicked//
+		change1();
+	}, false);
+var experience = document.getElementById("experience-click");
+experience.addEventListener('click', function(event){
+	change2();
+}, false);
+var work = document.getElementById("work-click");
+work.addEventListener('click', function(event) {
+		change3();
+	}, false);
+var contact = document.getElementById("contact-click");
+contact.addEventListener('click', function(event) {
+		change4();
+	}, false);
+var home = document.getElementById("home-click");
+home.addEventListener('click', function(event) {
+		change5();
+	}, false);
