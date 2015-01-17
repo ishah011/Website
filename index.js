32f5cc0b250cc396410495ca276b59326b3b61c1
Isheeta Shah <isheeta_s@yahoo.com>
2015-01-16 22:29:43 -0600

    changed titles

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
 var aboutBtn = document.getElementById("About");//gives each information element a variable//
+var edBtn = document.getElementById("Education");
 var expBtn = document.getElementById("Experience");
-var wrkBtn = document.getElementById("Work");
 var cntBtn = document.getElementById("Contact");
 var copy = document.getElementById("copyright");
 $(aboutBtn).hide();//hiding all the elements on load//
+$(edBtn).hide();
 $(expBtn).hide();
-$(wrkBtn).hide();
 $(cntBtn).hide();
 function change1(){//hide all the elements except for the About, fade that in at medium speed and change the background image//
 	$(aboutBtn).fadeIn("medium");
+	$(edBtn).hide();
 	$(expBtn).hide();
-	$(wrkBtn).hide();
 	$(cntBtn).hide();
 	$(copy).hide();
 	document.body.style.backgroundImage="url('https://raw.githubusercontent.com/ishah011/Website/gh-pages/images/Aboutbg.jpg')";
 }
 function change2(){//hide all the elements except for the Experience, fade that in at medium speed//
 	$(aboutBtn).hide();
-	$(expBtn).fadeIn("medium");
-	$(wrkBtn).hide();
+	$(edBtn).fadeIn("medium");
+	$(expBtn).hide();
 	$(cntBtn).hide();
 	$(copy).hide();
 	document.body.style.backgroundImage="url('https://raw.githubusercontent.com/ishah011/Website/gh-pages/images/Experiencebg.jpg')";
 }
 function change3(){//hide all the elements except for the Work, fade that in at medium speed//
-	$(expBtn).hide();
+	$(edBtn).hide();
 	$(aboutBtn).hide();
-	$(wrkBtn).fadeIn("medium");
+	$(expBtn).fadeIn("medium");
 	$(cntBtn).hide();
 	$(copy).hide();
 	document.body.style.backgroundImage="url('https://raw.githubusercontent.com/ishah011/Website/gh-pages/images/Workbg.jpg')";
 }
 function change4(){//hide all the elements except for the Contact, fade that in at medium speed//
-	$(expBtn).hide();
+	$(edBtn).hide();
 	$(aboutBtn).hide();
-	$(wrkBtn).hide();
+	$(expBtn).hide();
 	$(cntBtn).fadeIn("medium");
 	$(copy).hide();
 	document.body.style.backgroundImage="url('https://raw.githubusercontent.com/ishah011/Website/gh-pages/images/contactbg.jpg')";
 }
 function change5(){//hide all the elements and change the background image//
-	$(expBtn).hide();
+	$(edBtn).hide();
 	$(aboutBtn).hide();
-	$(wrkBtn).hide();
+	$(expBtn).hide();
 	$(cntBtn).hide();
 	$(copy).show();
 	document.body.style.backgroundImage="url('https://raw.githubusercontent.com/ishah011/Website/gh-pages/images/homebg.jpg')";
@@ -51,12 +51,12 @@ var about = document.getElementById("about-click");//create variable that is ass
 about.addEventListener('click', function(event) {//attach the change1 function to the about button; excutes it when about is clicked//
 		change1();
 	}, false);
-var experience = document.getElementById("experience-click");
-experience.addEventListener('click', function(event){
+var education = document.getElementById("education-click");
+education.addEventListener('click', function(event){
 	change2();
 }, false);
-var work = document.getElementById("work-click");
-work.addEventListener('click', function(event) {
+var experience = document.getElementById("experience-click");
+experience.addEventListener('click', function(event) {
 		change3();
 	}, false);
 var contact = document.getElementById("contact-click");
